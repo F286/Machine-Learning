df87a88de7bd9a9418b5c3f7336adee737aa93ab
Free Debreuil <freedebreuil@gmail.com>
2016-04-08 19:51:07 +0900

    graph different axes

--- Assets/Content/Scene Gradient/Scripts/CONTENT_Gradient.cs
+++ Assets/Content/Scene Gradient/Scripts/CONTENT_Gradient.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class CONTENT_Gradient : MonoBehaviour 
 {
@@ -11,10 +12,12 @@ public class CONTENT_Gradient : MonoBehaviour
     public float analyticalGradient;
     public float numericalGradient;
 
+    public static Vector4 TestAxis = new Vector4(1, 0, 0, 0);
+
     public void Update()
     {
         value = Forward(input);
-        analyticalGradient = Backward(input);
+        analyticalGradient = Backward(input).MaskSum(TestAxis);
         numericalGradient = BackwardNumerical(input);
 
         Debug.DrawLine(new Vector3(-5, 0, 0), new Vector3(5, 0, 0), Color.black);
@@ -27,59 +30,68 @@ public class CONTENT_Gradient : MonoBehaviour
 
         for (float i = -5; i < 5; i += 0.1f)
         {
-            var a = new Vector4(i, 1, 0, 0);
-            var b = new Vector4(i + 0.1f, 1, 0, 0);
+            var a = input.InverseMask(TestAxis) + TestAxis * i;
+            var b = input.InverseMask(TestAxis) + TestAxis * (i + 0.1f);
+
             Debug.DrawLine(new Vector3(i, Forward(a)), new Vector3(i + 0.1f, Forward(b)), Color.black);
             Debug.DrawLine(new Vector3(i, BackwardNumerical(a)), new Vector3(i + 0.1f, BackwardNumerical(b)), g);
-            Debug.DrawLine(new Vector3(i, Backward(a)), new Vector3(i + 0.1f, Backward(b)), r);
+            Debug.DrawLine(new Vector3(i, Backward(a).MaskSum(TestAxis)), new Vector3(i + 0.1f, Backward(b).MaskSum(TestAxis)), r);
         }
     }
 
     public static float Forward(Vector4 input)
     {
-//        Vector4 value = Vector4.zero;
         float value = 0;
         // apply weights and add inputs
-        value = input.x * input.x + input.y * 0.5f + input.z * 0.3f;
+        value = input.x * 1.6f + input.y * 0.5f + input.z * 0.3f + input.w;
 
         // easing curve between -1 and 1
         value = (float)System.Math.Tanh(value);
 
         return value;
     }
-    public static float Backward(Vector4 input)
+    public static Vector4 Backward(Vector4 input)
     {
         Vector4 gradient = Vector4.one;
 
         // apply weights and add inputs
-        gradient.x *= 2 * input.x;
+        gradient.x *= 1.6f;
+        gradient.y *= 0.5f;
+        gradient.z *= 0.3f;
+        gradient.w = 1f;
 
         // calculate what value was right before tanh function (can cache this)
-        var value = input.x * input.x + input.y * 0.5f + input.z * 0.3f;
+        var value = input.x * 1.6f + input.y * 0.5f + input.z * 0.3f + input.w;
 
         // easing curve between -1 and 1
-        gradient.x *= 1 - ((float)System.Math.Tanh(value)).Squared();
+        gradient *= 1 - ((float)System.Math.Tanh(value)).Squared();
 
-        return gradient.x;
+        return gradient;
     }
-//    public static float Forward(float value)
-//    {
-//        value *= 2;
-//        value = (Mathf.Exp(value) - Mathf.Exp(-value)) / (Mathf.Exp(value) + Mathf.Exp(-value));
-//        return value;
-//    }
-//    public static float Backward(float gradient)
-//    {
-//        gradient *= 2;
-//        gradient = 2 / (Mathf.Exp(gradient) + Mathf.Exp(-gradient));
-//        gradient *= gradient;
-//        gradient *= 2;
-//        return gradient;
-//    }
     public static float BackwardNumerical(Vector4 input)
     {
         var a = Forward(input);
-        var b = Forward(input + new Vector4(NumericalStepSize, 0));
+        var b = Forward(input + NumericalStepSize * TestAxis);
         return (b - a) / NumericalStepSize;
+    }
+
+    public void OnGUI()
+    {
+        if (GUILayout.Button("X"))
+        {
+            TestAxis = new Vector4(1, 0, 0, 0);
+        }
+        if (GUILayout.Button("Y"))
+        {
+            TestAxis = new Vector4(0, 1, 0, 0);
+        }
+        if (GUILayout.Button("Z"))
+        {
+            TestAxis = new Vector4(0, 0, 1, 0);
+        }
+        if (GUILayout.Button("W"))
+        {
+            TestAxis = new Vector4(0, 0, 0, 1);
+        }
     }
 }
--- Assets/Standard Assets/Scripts/Core.cs
+++ Assets/Standard Assets/Scripts/Core.cs
@@ -7,4 +7,22 @@ public static class Core
     {
         return v * v;
     }
+
+    public static Vector4 Mask(this Vector4 value, Vector4 mask)
+    {
+        return new Vector4(value.x * mask.x, value.y * mask.y, value.z * mask.z, value.w * mask.w);
+    }
+
+    public static Vector4 InverseMask(this Vector4 value, Vector4 mask)
+    {
+        mask.x = Mathf.Max(mask.x - 1f, 0);
+        mask.y = Mathf.Max(mask.y - 1f, 0);
+        mask.z = Mathf.Max(mask.z - 1f, 0);
+        mask.w = Mathf.Max(mask.w - 1f, 0);
+        return new Vector4(value.x * mask.x, value.y * mask.y, value.z * mask.z, value.w * mask.w);
+    }
+    public static float MaskSum(this Vector4 value, Vector4 mask)
+    {
+        return value.x * mask.x + value.y * mask.y + value.z * mask.z + value.w * mask.w;
+    }
 }
