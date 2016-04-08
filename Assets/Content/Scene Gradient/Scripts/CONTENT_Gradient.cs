5b7b2f9d7bc8c47f3191ca4e43443623946e6693
Free Debreuil <freedebreuil@gmail.com>
2016-04-08 23:20:25 +0900

    bias

--- Assets/Content/Scene Gradient/Scripts/CONTENT_Gradient.cs
+++ Assets/Content/Scene Gradient/Scripts/CONTENT_Gradient.cs
@@ -7,6 +7,7 @@ public class CONTENT_Gradient : MonoBehaviour
     const float NumericalStepSize = 0.00001f;
 
     public Vector4 input;
+    public Vector4 weights;
 
     public float value;
     public float analyticalGradient;
@@ -16,9 +17,9 @@ public class CONTENT_Gradient : MonoBehaviour
 
     public void Update()
     {
-        value = Forward(input);
-        analyticalGradient = Backward(input).MaskSum(TestAxis);
-        numericalGradient = BackwardNumerical(input);
+        value = Forward(input, weights);
+        analyticalGradient = Backward(input, weights).MaskSum(TestAxis);
+        numericalGradient = BackwardNumerical(input, weights);
 
         Debug.DrawLine(new Vector3(-5, 0, 0), new Vector3(5, 0, 0), Color.black);
         Debug.DrawLine(new Vector3(0, -5, 0), new Vector3(0, 5, 0), Color.black);
@@ -28,51 +29,52 @@ public class CONTENT_Gradient : MonoBehaviour
         var r = Color.red;
         r.a = 0.8f;
 
-        print(input.InverseMask(TestAxis));
+//        print(input.InverseMask(TestAxis));
         for (float i = -5; i < 5; i += 0.1f)
         {
             var a = input.InverseMask(TestAxis) + TestAxis * i;
             var b = input.InverseMask(TestAxis) + TestAxis * (i + 0.1f);
 
-            Debug.DrawLine(new Vector3(i, Forward(a)), new Vector3(i + 0.1f, Forward(b)), Color.black);
-            Debug.DrawLine(new Vector3(i, BackwardNumerical(a)), new Vector3(i + 0.1f, BackwardNumerical(b)), g);
-            Debug.DrawLine(new Vector3(i, Backward(a).MaskSum(TestAxis)), new Vector3(i + 0.1f, Backward(b).MaskSum(TestAxis)), r);
+            Debug.DrawLine(new Vector3(i, Forward(a, weights)), new Vector3(i + 0.1f, Forward(b, weights)), Color.black);
+            Debug.DrawLine(new Vector3(i, BackwardNumerical(a, weights)), new Vector3(i + 0.1f, BackwardNumerical(b, weights)), g);
+            Debug.DrawLine(new Vector3(i, Backward(a, weights).MaskSum(TestAxis)), new Vector3(i + 0.1f, Backward(b, weights).MaskSum(TestAxis)), r);
         }
     }
 
-    public static float Forward(Vector4 input)
+    public static float Forward(Vector4 input, Vector4 weights)
     {
         float value = 0;
         // apply weights and add inputs
-        value = input.x * 1.6f + input.y * 0.5f + input.z * 0.3f + input.w;
+        value = input.x * weights.x + input.y * weights.y + input.z * weights.z + input.w;//weights.w;
 
         // easing curve between -1 and 1
         value = (float)System.Math.Tanh(value);
 
         return value;
     }
-    public static Vector4 Backward(Vector4 input)
+    public static Vector4 Backward(Vector4 input, Vector4 weights)
     {
         Vector4 gradient = Vector4.one;
 
         // apply weights and add inputs
-        gradient.x *= 1.6f;
-        gradient.y *= 0.5f;
-        gradient.z *= 0.3f;
-        gradient.w = 1f;
+        gradient.x *= weights.x;
+        gradient.y *= weights.y;
+        gradient.z *= weights.z;
+        gradient.w *= 1;
+//        gradient.w *= 1;//weights.w;
 
         // calculate what value was right before function (can cache this)
-        var value = input.x * 1.6f + input.y * 0.5f + input.z * 0.3f + input.w;
+        var value = input.x * weights.x + input.y * weights.y + input.z * weights.z + input.w;//weights.w;
 
         // easing curve between -1 and 1
         gradient *= 1 - ((float)System.Math.Tanh(value)).Squared();
 
         return gradient;
     }
-    public static float BackwardNumerical(Vector4 input)
+    public static float BackwardNumerical(Vector4 input, Vector4 weights)
     {
-        var a = Forward(input);
-        var b = Forward(input + NumericalStepSize * TestAxis);
+        var a = Forward(input, weights);
+        var b = Forward(input + NumericalStepSize * TestAxis, weights);
         return (b - a) / NumericalStepSize;
     }
 
