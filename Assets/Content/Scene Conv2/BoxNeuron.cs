3b7beb71b516f825ff059e2b91b3c3ff7120f3b5
Free Debreuil <freedebreuil@gmail.com>
2016-05-11 20:17:26 +0900

    working online rendering

---
+++ Assets/Content/Scene Conv2/BoxNeuron.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxNeuron : MonoBehaviour 
+{
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        ManagerConv2.inst.AddConnection(gameObject, coll.gameObject);
+    }
+}
--- Assets/Content/Scene Conv2/ManagerConv2.cs
+++ Assets/Content/Scene Conv2/ManagerConv2.cs
@@ -1,23 +1,86 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ManagerConv2 : MonoBehaviour 
 {
-//    public class Neuron
-//    {
-//        public Vector2 position;
-////        public  
-//    }
+//    const int particlesPerConnection = 128;
 
-	public void Start () 
+    public List<Connection> connections = new List<Connection>();
+
+    ParticleSystem.Particle[] particles = new ParticleSystem.Particle[1000];
+    public ParticleSystem lines;
+
+    public void Start()
     {
-//        Matrix input1 = new Matrix(16, 16);
-//
-//        print(input1);
-	}
-	
-//	public void Update () 
-//    {
-//        Debug.DrawLine(new Vector3(0, 0), new Vector3(1, 0), Color.blue);
-//	}
+        lines.Emit(1000);
+    }
+
+    public void AddConnection(GameObject a, GameObject b)
+    {
+        var add = new Connection(a, b);
+        if (!connections.Contains(add))
+        {
+            connections.Add(add);
+        }
+    }
+
+    public void Update()
+    {
+        var size = 0;
+        lines.GetParticles(particles);
+
+        for (int i = 0; i < connections.Count; i++)
+        {
+            var a = connections[i].a.transform.position;
+            var b = connections[i].b.transform.position;
+            var d = b - a;
+            var dNorm = d.normalized;
+            var dPerp = new Vector3(-dNorm.y, dNorm.x);
+            var mag = d.magnitude;
+            a += dNorm * 0.3f;
+            b -= dNorm * 0.3f;
+            var particlesPerConnection = Mathf.RoundToInt(mag * 12);
+            for (int c = 0; c < particlesPerConnection; c++) 
+            {
+                var index = size + c;
+                particles[index].lifetime = 1;
+                var l = c / (float)(particlesPerConnection - 1);
+                particles[index].position = Vector3.Lerp(a, b, l)
+                    + dPerp * Mathf.Sin(mag * -1f + mag * l * 5.25f + Time.time * 9) * 0.125f;
+            }
+            size += particlesPerConnection;
+        }
+
+        lines.SetParticles(particles, size);
+    }
+
+    public static ManagerConv2 inst
+    {
+        get
+        {
+            return GameObject.FindGameObjectWithTag("manager").GetComponent<ManagerConv2>();
+        }
+    }
+}
+
+[System.Serializable]
+public struct Connection
+{
+    public GameObject a;
+    public GameObject b;
+
+    public Connection(GameObject a, GameObject b)
+    {
+        if (a.GetInstanceID() < b.GetInstanceID())
+        {
+            this.a = a;
+            this.b = b;
+        }
+        else
+        {
+            this.b = a;
+            this.a = b;
+        }
+    }
 }
